0aa67b3d746141dd1b3857327545808082623b88
karthik <karthikpsreekumar2@gmail.com>
2022-03-25 20:18:40 +0530

    add files

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import "./App.css";
+import Haha from "./Haha"
+function App() {
+  return (
+    <div className="App">
+    <div className="bg-blue-200"><Haha/></div>
+    mandan njan
+
+      
+    </div>
+  );
+}
+
+export default App;
