9e2c245fdc205c30197f4f9ace06ff3849a88b0b
karthik <karthikpsreekumar2@gmail.com>
2022-03-25 20:57:19 +0530

    add poocha

--- src/App.js
+++ src/App.js
@@ -1,13 +1,46 @@
-
+import axios from "axios";
+import { useState } from "react";
 import "./App.css";
-import Haha from "./Haha"
 function App() {
+  console.log("kartthootohik");
+
+  const [products, setproduct] = useState([]);
+
+  function karthikclick() {
+    axios.get("https://fakestoreapi.com/products").then((kart) => {
+      setproduct(kart.data);
+    });
+  }
+
   return (
     <div className="App">
-    <div className="bg-blue-200"><Haha/></div>
-    mandan njan
-
-      
+      mandan njan vasu gundoos thooti
+      <button
+        style={{ backgroundColor: "rebeccapurple" }}
+        onClick={karthikclick}
+      >
+        clkartcick
+      </button>
+      {products.length != 0 &&
+        products.map((e) => {
+          return (
+            <>
+              <div>
+                <h1>name is {e.title}</h1>
+                <img
+                  style={{ height: "100px", width: "100px" }}
+                  src={e.image}
+                />
+              </div>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+              <br></br>
+            </>
+          );
+        })}
     </div>
   );
 }
