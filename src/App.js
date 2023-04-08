e9eb090c4669a38df2a100d6a4a0df51b3512d2d
karthik ps <karthikpsreekumar2@gmail.com>
2023-04-08 21:36:24 +0530

    latest

--- src/App.js
+++ src/App.js
@@ -1,44 +1,84 @@
 import axios from "axios";
 import { useState } from "react";
 import "./App.css";
+import styled from "styled-components";
+import Mandan from "./Mandan";
+
+const Main = styled.div`
+  background-color: wheat;
+  .red {
+    background-color: green;
+  }
+  .mandan {
+    backdrop-filter: calc();
+    background-color: darkblue;
+    color: aqua;
+  }
+  h1 {
+    text-align: center;
+  }
+  .submit {
+    background-color: darkcyan;
+  }
+`;
+
 function App() {
   console.log("kartthootohik");
 
   const [products, setproduct] = useState([]);
 
-  function karthikclick() {
+  function abatton() {
     axios.get("https://fakestoreapi.com/products").then((kart) => {
       setproduct(kart.data);
     });
   }
 
+  const [input, setInput] = useState("");
+
   return (
-    <div className="App">
-      mandan njan vasu gundoos thooti
-      <button style={{ backgroundColor: "red" }} onClick={karthikclick}>
-        clkartcick
+    <Main>
+      mandan njan vasu gundoos thooti pupoos
+      <button className="red" onClick={abatton}>
+        clickme
       </button>
-      {products.length != 0 &&
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          alert("ഇപ്പൊ ശെരിയാക്കി തരാം (●'◡'●)");
+        }}
+      >
+        <input
+          required
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          placeholder="Any problems ?   "
+        />
+        <button type="submit" className="submit">
+          submit
+        </button>
+      </form>
+      {products.length !== 0 &&
         products.map((e) => {
           return (
             <>
               <div>
                 <h1>name is {e.title}</h1>
+                <h1>price is {Math.floor(Math.random() * 1000)} </h1>
                 <img
-                  style={{ height: "100px", width: "100px" }}
+                  style={{
+                    height: "100px",
+                    width: "100px",
+                    margin: "0px auto",
+                  }}
                   src={e.image}
                 />
+                <h1>stock : {Math.floor(Math.random() * 100)}</h1>
               </div>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
-              <br></br>
               <br></br>
             </>
           );
         })}
-    </div>
+    </Main>
   );
 }
 
---
+++ src/Mandan.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Mandan() {
+  return <div className="mandan">I am a Mandan</div>;
+}
+
+export default Mandan;
